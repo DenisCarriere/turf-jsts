8d03683611723b60babb9ac1e03941b1ff6bdac2
Denis <carriere.denis@gmail.com>
2017-11-13 21:53:08 -0800

    Add buffer builder

--- src/org/locationtech/jts/operation/buffer/BufferBuilder.js
+++ src/org/locationtech/jts/operation/buffer/BufferBuilder.js
@@ -1,152 +1,150 @@
-import Location from '../../geom/Location';
-import BufferSubgraph from './BufferSubgraph';
-import PolygonBuilder from '../overlay/PolygonBuilder';
-import GeometryFactory from '../../geom/GeometryFactory';
-import Position from '../../geomgraph/Position';
-import MCIndexNoder from '../../noding/MCIndexNoder';
-import OffsetCurveBuilder from './OffsetCurveBuilder';
-import extend from '../../../../../extend';
-import Collections from '../../../../../java/util/Collections';
-import SubgraphDepthLocater from './SubgraphDepthLocater';
-import OffsetCurveSetBuilder from './OffsetCurveSetBuilder';
-import Label from '../../geomgraph/Label';
-import OverlayNodeFactory from '../overlay/OverlayNodeFactory';
-import EdgeList from '../../geomgraph/EdgeList';
-import ArrayList from '../../../../../java/util/ArrayList';
-import RobustLineIntersector from '../../algorithm/RobustLineIntersector';
-import IntersectionAdder from '../../noding/IntersectionAdder';
-import Edge from '../../geomgraph/Edge';
-import PlanarGraph from '../../geomgraph/PlanarGraph';
-export default function BufferBuilder() {
-	this._bufParams = null;
-	this._workingPrecisionModel = null;
-	this._workingNoder = null;
-	this._geomFact = null;
-	this._graph = null;
-	this._edgeList = new EdgeList();
-	let bufParams = arguments[0];
-	this._bufParams = bufParams;
+import Location from '../../geom/Location'
+import BufferSubgraph from './BufferSubgraph'
+import PolygonBuilder from '../overlay/PolygonBuilder'
+import GeometryFactory from '../../geom/GeometryFactory'
+import Position from '../../geomgraph/Position'
+import MCIndexNoder from '../../noding/MCIndexNoder'
+import OffsetCurveBuilder from './OffsetCurveBuilder'
+import Collections from '../../../../../java/util/Collections'
+import SubgraphDepthLocater from './SubgraphDepthLocater'
+import OffsetCurveSetBuilder from './OffsetCurveSetBuilder'
+import Label from '../../geomgraph/Label'
+import OverlayNodeFactory from '../overlay/OverlayNodeFactory'
+import EdgeList from '../../geomgraph/EdgeList'
+import ArrayList from '../../../../../java/util/ArrayList'
+import RobustLineIntersector from '../../algorithm/RobustLineIntersector'
+import IntersectionAdder from '../../noding/IntersectionAdder'
+import Edge from '../../geomgraph/Edge'
+import PlanarGraph from '../../geomgraph/PlanarGraph'
+
+export default class BufferBuilder {
+  constructor (bufParams) {
+    this._workingPrecisionModel = null
+    this._workingNoder = null
+    this._geomFact = null
+    this._graph = null
+    this._edgeList = new EdgeList()
+    this._bufParams = bufParams || null
+  }
+  setWorkingPrecisionModel (pm) {
+    this._workingPrecisionModel = pm
+  }
+  insertUniqueEdge (e) {
+    const existingEdge = this._edgeList.findEqualEdge(e)
+    if (existingEdge !== null) {
+      const existingLabel = existingEdge.getLabel()
+      let labelToMerge = e.getLabel()
+      if (!existingEdge.isPointwiseEqual(e)) {
+        labelToMerge = new Label(e.getLabel())
+        labelToMerge.flip()
+      }
+      existingLabel.merge(labelToMerge)
+      const mergeDelta = BufferBuilder.depthDelta(labelToMerge)
+      const existingDelta = existingEdge.getDepthDelta()
+      const newDelta = existingDelta + mergeDelta
+      existingEdge.setDepthDelta(newDelta)
+    } else {
+      this._edgeList.add(e)
+      e.setDepthDelta(BufferBuilder.depthDelta(e.getLabel()))
+    }
+  }
+  buildSubgraphs (subgraphList, polyBuilder) {
+    const processedGraphs = new ArrayList()
+    for (const i = subgraphList.iterator(); i.hasNext();) {
+      const subgraph = i.next()
+      const p = subgraph.getRightmostCoordinate()
+      const locater = new SubgraphDepthLocater(processedGraphs)
+      const outsideDepth = locater.getDepth(p)
+      subgraph.computeDepth(outsideDepth)
+      subgraph.findResultEdges()
+      processedGraphs.add(subgraph)
+      polyBuilder.add(subgraph.getDirectedEdges(), subgraph.getNodes())
+    }
+  }
+  createSubgraphs (graph) {
+    const subgraphList = new ArrayList()
+    for (const i = graph.getNodes().iterator(); i.hasNext();) {
+      const node = i.next()
+      if (!node.isVisited()) {
+        const subgraph = new BufferSubgraph()
+        subgraph.create(node)
+        subgraphList.add(subgraph)
+      }
+    }
+    Collections.sort(subgraphList, Collections.reverseOrder())
+    return subgraphList
+  }
+  createEmptyResultGeometry () {
+    const emptyGeom = this._geomFact.createPolygon()
+    return emptyGeom
+  }
+  getNoder (precisionModel) {
+    if (this._workingNoder !== null) return this._workingNoder
+    const noder = new MCIndexNoder()
+    const li = new RobustLineIntersector()
+    li.setPrecisionModel(precisionModel)
+    noder.setSegmentIntersector(new IntersectionAdder(li))
+    return noder
+  }
+  buffer (g, distance) {
+    let precisionModel = this._workingPrecisionModel
+    if (precisionModel === null) precisionModel = g.getPrecisionModel()
+    this._geomFact = g.getFactory()
+    const curveBuilder = new OffsetCurveBuilder(precisionModel, this._bufParams)
+    const curveSetBuilder = new OffsetCurveSetBuilder(g, distance, curveBuilder)
+    const bufferSegStrList = curveSetBuilder.getCurves()
+    if (bufferSegStrList.size() <= 0) {
+      return this.createEmptyResultGeometry()
+    }
+    this.computeNodedEdges(bufferSegStrList, precisionModel)
+    this._graph = new PlanarGraph(new OverlayNodeFactory())
+    this._graph.addEdges(this._edgeList.getEdges())
+    const subgraphList = this.createSubgraphs(this._graph)
+    const polyBuilder = new PolygonBuilder(this._geomFact)
+    this.buildSubgraphs(subgraphList, polyBuilder)
+    const resultPolyList = polyBuilder.getPolygons()
+    if (resultPolyList.size() <= 0) {
+      return this.createEmptyResultGeometry()
+    }
+    const resultGeom = this._geomFact.buildGeometry(resultPolyList)
+    return resultGeom
+  }
+  computeNodedEdges (bufferSegStrList, precisionModel) {
+    const noder = this.getNoder(precisionModel)
+    noder.computeNodes(bufferSegStrList)
+    const nodedSegStrings = noder.getNodedSubstrings()
+    for (const i = nodedSegStrings.iterator(); i.hasNext();) {
+      const segStr = i.next()
+      const pts = segStr.getCoordinates()
+      if (pts.length === 2 && pts[0].equals2D(pts[1])) continue
+      const oldLabel = segStr.getData()
+      const edge = new Edge(segStr.getCoordinates(), new Label(oldLabel))
+      this.insertUniqueEdge(edge)
+    }
+  }
+  setNoder (noder) {
+    this._workingNoder = noder
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return BufferBuilder
+  }
+  static depthDelta (label) {
+    const lLoc = label.getLocation(0, Position.LEFT)
+    const rLoc = label.getLocation(0, Position.RIGHT)
+    if (lLoc === Location.INTERIOR && rLoc === Location.EXTERIOR) return 1; else if (lLoc === Location.EXTERIOR && rLoc === Location.INTERIOR) return -1
+    return 0
+  }
+  static convertSegStrings (it) {
+    const fact = new GeometryFactory()
+    const lines = new ArrayList()
+    while (it.hasNext()) {
+      const ss = it.next()
+      const line = fact.createLineString(ss.getCoordinates())
+      lines.add(line)
+    }
+    return fact.buildGeometry(lines)
+  }
 }
-extend(BufferBuilder.prototype, {
-	setWorkingPrecisionModel: function (pm) {
-		this._workingPrecisionModel = pm;
-	},
-	insertUniqueEdge: function (e) {
-		var existingEdge = this._edgeList.findEqualEdge(e);
-		if (existingEdge !== null) {
-			var existingLabel = existingEdge.getLabel();
-			var labelToMerge = e.getLabel();
-			if (!existingEdge.isPointwiseEqual(e)) {
-				labelToMerge = new Label(e.getLabel());
-				labelToMerge.flip();
-			}
-			existingLabel.merge(labelToMerge);
-			var mergeDelta = BufferBuilder.depthDelta(labelToMerge);
-			var existingDelta = existingEdge.getDepthDelta();
-			var newDelta = existingDelta + mergeDelta;
-			existingEdge.setDepthDelta(newDelta);
-		} else {
-			this._edgeList.add(e);
-			e.setDepthDelta(BufferBuilder.depthDelta(e.getLabel()));
-		}
-	},
-	buildSubgraphs: function (subgraphList, polyBuilder) {
-		var processedGraphs = new ArrayList();
-		for (var i = subgraphList.iterator(); i.hasNext(); ) {
-			var subgraph = i.next();
-			var p = subgraph.getRightmostCoordinate();
-			var locater = new SubgraphDepthLocater(processedGraphs);
-			var outsideDepth = locater.getDepth(p);
-			subgraph.computeDepth(outsideDepth);
-			subgraph.findResultEdges();
-			processedGraphs.add(subgraph);
-			polyBuilder.add(subgraph.getDirectedEdges(), subgraph.getNodes());
-		}
-	},
-	createSubgraphs: function (graph) {
-		var subgraphList = new ArrayList();
-		for (var i = graph.getNodes().iterator(); i.hasNext(); ) {
-			var node = i.next();
-			if (!node.isVisited()) {
-				var subgraph = new BufferSubgraph();
-				subgraph.create(node);
-				subgraphList.add(subgraph);
-			}
-		}
-		Collections.sort(subgraphList, Collections.reverseOrder());
-		return subgraphList;
-	},
-	createEmptyResultGeometry: function () {
-		var emptyGeom = this._geomFact.createPolygon();
-		return emptyGeom;
-	},
-	getNoder: function (precisionModel) {
-		if (this._workingNoder !== null) return this._workingNoder;
-		var noder = new MCIndexNoder();
-		var li = new RobustLineIntersector();
-		li.setPrecisionModel(precisionModel);
-		noder.setSegmentIntersector(new IntersectionAdder(li));
-		return noder;
-	},
-	buffer: function (g, distance) {
-		var precisionModel = this._workingPrecisionModel;
-		if (precisionModel === null) precisionModel = g.getPrecisionModel();
-		this._geomFact = g.getFactory();
-		var curveBuilder = new OffsetCurveBuilder(precisionModel, this._bufParams);
-		var curveSetBuilder = new OffsetCurveSetBuilder(g, distance, curveBuilder);
-		var bufferSegStrList = curveSetBuilder.getCurves();
-		if (bufferSegStrList.size() <= 0) {
-			return this.createEmptyResultGeometry();
-		}
-		this.computeNodedEdges(bufferSegStrList, precisionModel);
-		this._graph = new PlanarGraph(new OverlayNodeFactory());
-		this._graph.addEdges(this._edgeList.getEdges());
-		var subgraphList = this.createSubgraphs(this._graph);
-		var polyBuilder = new PolygonBuilder(this._geomFact);
-		this.buildSubgraphs(subgraphList, polyBuilder);
-		var resultPolyList = polyBuilder.getPolygons();
-		if (resultPolyList.size() <= 0) {
-			return this.createEmptyResultGeometry();
-		}
-		var resultGeom = this._geomFact.buildGeometry(resultPolyList);
-		return resultGeom;
-	},
-	computeNodedEdges: function (bufferSegStrList, precisionModel) {
-		var noder = this.getNoder(precisionModel);
-		noder.computeNodes(bufferSegStrList);
-		var nodedSegStrings = noder.getNodedSubstrings();
-		for (var i = nodedSegStrings.iterator(); i.hasNext(); ) {
-			var segStr = i.next();
-			var pts = segStr.getCoordinates();
-			if (pts.length === 2 && pts[0].equals2D(pts[1])) continue;
-			var oldLabel = segStr.getData();
-			var edge = new Edge(segStr.getCoordinates(), new Label(oldLabel));
-			this.insertUniqueEdge(edge);
-		}
-	},
-	setNoder: function (noder) {
-		this._workingNoder = noder;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return BufferBuilder;
-	}
-});
-BufferBuilder.depthDelta = function (label) {
-	var lLoc = label.getLocation(0, Position.LEFT);
-	var rLoc = label.getLocation(0, Position.RIGHT);
-	if (lLoc === Location.INTERIOR && rLoc === Location.EXTERIOR) return 1; else if (lLoc === Location.EXTERIOR && rLoc === Location.INTERIOR) return -1;
-	return 0;
-};
-BufferBuilder.convertSegStrings = function (it) {
-	var fact = new GeometryFactory();
-	var lines = new ArrayList();
-	while (it.hasNext()) {
-		var ss = it.next();
-		var line = fact.createLineString(ss.getCoordinates());
-		lines.add(line);
-	}
-	return fact.buildGeometry(lines);
-};
--- src/org/locationtech/jts/operation/buffer/BufferInputLineSimplifier.js
+++ src/org/locationtech/jts/operation/buffer/BufferInputLineSimplifier.js
@@ -1,100 +1,98 @@
-import CGAlgorithms from '../../algorithm/CGAlgorithms';
-import CoordinateList from '../../geom/CoordinateList';
-import extend from '../../../../../extend';
-export default function BufferInputLineSimplifier() {
-	this._inputLine = null;
-	this._distanceTol = null;
-	this._isDeleted = null;
-	this._angleOrientation = CGAlgorithms.COUNTERCLOCKWISE;
-	let inputLine = arguments[0];
-	this._inputLine = inputLine;
+import CGAlgorithms from '../../algorithm/CGAlgorithms'
+import CoordinateList from '../../geom/CoordinateList'
+
+export default class BufferInputLineSimplifier {
+  constructor (inputLine) {
+    this._distanceTol = null
+    this._isDeleted = null
+    this._angleOrientation = CGAlgorithms.COUNTERCLOCKWISE
+    this._inputLine = inputLine || null
+  }
+  isDeletable (i0, i1, i2, distanceTol) {
+    const p0 = this._inputLine[i0]
+    const p1 = this._inputLine[i1]
+    const p2 = this._inputLine[i2]
+    if (!this.isConcave(p0, p1, p2)) return false
+    if (!this.isShallow(p0, p1, p2, distanceTol)) return false
+    return this.isShallowSampled(p0, p1, i0, i2, distanceTol)
+  }
+  deleteShallowConcavities () {
+    let index = 1
+    // const maxIndex = this._inputLine.length - 1
+    let midIndex = this.findNextNonDeletedIndex(index)
+    let lastIndex = this.findNextNonDeletedIndex(midIndex)
+    let isChanged = false
+    while (lastIndex < this._inputLine.length) {
+      let isMiddleVertexDeleted = false
+      if (this.isDeletable(index, midIndex, lastIndex, this._distanceTol)) {
+        this._isDeleted[midIndex] = BufferInputLineSimplifier.DELETE
+        isMiddleVertexDeleted = true
+        isChanged = true
+      }
+      if (isMiddleVertexDeleted) index = lastIndex; else index = midIndex
+      midIndex = this.findNextNonDeletedIndex(index)
+      lastIndex = this.findNextNonDeletedIndex(midIndex)
+    }
+    return isChanged
+  }
+  isShallowConcavity (p0, p1, p2, distanceTol) {
+    const orientation = CGAlgorithms.computeOrientation(p0, p1, p2)
+    const isAngleToSimplify = orientation === this._angleOrientation
+    if (!isAngleToSimplify) return false
+    const dist = CGAlgorithms.distancePointLine(p1, p0, p2)
+    return dist < distanceTol
+  }
+  isShallowSampled (p0, p2, i0, i2, distanceTol) {
+    let inc = Math.trunc((i2 - i0) / BufferInputLineSimplifier.NUM_PTS_TO_CHECK)
+    if (inc <= 0) inc = 1
+    for (let i = i0; i < i2; i += inc) {
+      if (!this.isShallow(p0, p2, this._inputLine[i], distanceTol)) return false
+    }
+    return true
+  }
+  isConcave (p0, p1, p2) {
+    const orientation = CGAlgorithms.computeOrientation(p0, p1, p2)
+    const isConcave = orientation === this._angleOrientation
+    return isConcave
+  }
+  simplify (distanceTol) {
+    this._distanceTol = Math.abs(distanceTol)
+    if (distanceTol < 0) this._angleOrientation = CGAlgorithms.CLOCKWISE
+    this._isDeleted = new Array(this._inputLine.length).fill(null)
+    let isChanged = false
+    do {
+      isChanged = this.deleteShallowConcavities()
+    } while (isChanged)
+    return this.collapseLine()
+  }
+  findNextNonDeletedIndex (index) {
+    let next = index + 1
+    while (next < this._inputLine.length && this._isDeleted[next] === BufferInputLineSimplifier.DELETE) next++
+    return next
+  }
+  isShallow (p0, p1, p2, distanceTol) {
+    const dist = CGAlgorithms.distancePointLine(p1, p0, p2)
+    return dist < distanceTol
+  }
+  collapseLine () {
+    const coordList = new CoordinateList()
+    for (let i = 0; i < this._inputLine.length; i++) {
+      if (this._isDeleted[i] !== BufferInputLineSimplifier.DELETE) coordList.add(this._inputLine[i])
+    }
+    return coordList.toCoordinateArray()
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return BufferInputLineSimplifier
+  }
+  static simplify (inputLine, distanceTol) {
+    const simp = new BufferInputLineSimplifier(inputLine)
+    return simp.simplify(distanceTol)
+  }
+  static get INIT () { return 0 }
+  static get DELETE () { return 1 }
+  static get KEEP () { return 1 }
+  static get NUM_PTS_TO_CHECK () { return 10 }
 }
-extend(BufferInputLineSimplifier.prototype, {
-	isDeletable: function (i0, i1, i2, distanceTol) {
-		var p0 = this._inputLine[i0];
-		var p1 = this._inputLine[i1];
-		var p2 = this._inputLine[i2];
-		if (!this.isConcave(p0, p1, p2)) return false;
-		if (!this.isShallow(p0, p1, p2, distanceTol)) return false;
-		return this.isShallowSampled(p0, p1, i0, i2, distanceTol);
-	},
-	deleteShallowConcavities: function () {
-		var index = 1;
-		var maxIndex = this._inputLine.length - 1;
-		var midIndex = this.findNextNonDeletedIndex(index);
-		var lastIndex = this.findNextNonDeletedIndex(midIndex);
-		var isChanged = false;
-		while (lastIndex < this._inputLine.length) {
-			var isMiddleVertexDeleted = false;
-			if (this.isDeletable(index, midIndex, lastIndex, this._distanceTol)) {
-				this._isDeleted[midIndex] = BufferInputLineSimplifier.DELETE;
-				isMiddleVertexDeleted = true;
-				isChanged = true;
-			}
-			if (isMiddleVertexDeleted) index = lastIndex; else index = midIndex;
-			midIndex = this.findNextNonDeletedIndex(index);
-			lastIndex = this.findNextNonDeletedIndex(midIndex);
-		}
-		return isChanged;
-	},
-	isShallowConcavity: function (p0, p1, p2, distanceTol) {
-		var orientation = CGAlgorithms.computeOrientation(p0, p1, p2);
-		var isAngleToSimplify = orientation === this._angleOrientation;
-		if (!isAngleToSimplify) return false;
-		var dist = CGAlgorithms.distancePointLine(p1, p0, p2);
-		return dist < distanceTol;
-	},
-	isShallowSampled: function (p0, p2, i0, i2, distanceTol) {
-		var inc = Math.trunc((i2 - i0) / BufferInputLineSimplifier.NUM_PTS_TO_CHECK);
-		if (inc <= 0) inc = 1;
-		for (var i = i0; i < i2; i += inc) {
-			if (!this.isShallow(p0, p2, this._inputLine[i], distanceTol)) return false;
-		}
-		return true;
-	},
-	isConcave: function (p0, p1, p2) {
-		var orientation = CGAlgorithms.computeOrientation(p0, p1, p2);
-		var isConcave = orientation === this._angleOrientation;
-		return isConcave;
-	},
-	simplify: function (distanceTol) {
-		this._distanceTol = Math.abs(distanceTol);
-		if (distanceTol < 0) this._angleOrientation = CGAlgorithms.CLOCKWISE;
-		this._isDeleted = new Array(this._inputLine.length).fill(null);
-		var isChanged = false;
-		do {
-			isChanged = this.deleteShallowConcavities();
-		} while (isChanged);
-		return this.collapseLine();
-	},
-	findNextNonDeletedIndex: function (index) {
-		var next = index + 1;
-		while (next < this._inputLine.length && this._isDeleted[next] === BufferInputLineSimplifier.DELETE) next++;
-		return next;
-	},
-	isShallow: function (p0, p1, p2, distanceTol) {
-		var dist = CGAlgorithms.distancePointLine(p1, p0, p2);
-		return dist < distanceTol;
-	},
-	collapseLine: function () {
-		var coordList = new CoordinateList();
-		for (var i = 0; i < this._inputLine.length; i++) {
-			if (this._isDeleted[i] !== BufferInputLineSimplifier.DELETE) coordList.add(this._inputLine[i]);
-		}
-		return coordList.toCoordinateArray();
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return BufferInputLineSimplifier;
-	}
-});
-BufferInputLineSimplifier.simplify = function (inputLine, distanceTol) {
-	var simp = new BufferInputLineSimplifier(inputLine);
-	return simp.simplify(distanceTol);
-};
-BufferInputLineSimplifier.INIT = 0;
-BufferInputLineSimplifier.DELETE = 1;
-BufferInputLineSimplifier.KEEP = 1;
-BufferInputLineSimplifier.NUM_PTS_TO_CHECK = 10;
