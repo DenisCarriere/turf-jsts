f0f36a65ba93a0443988199b864920493c75d63b
Denis <carriere.denis@gmail.com>
2017-11-22 19:57:20 -0800

    remove semi-colons

--- src/org/locationtech/jts/operation/union.js
+++ src/org/locationtech/jts/operation/union.js
@@ -1,5 +1,5 @@
-export { default as CascadedPolygonUnion } from './union/CascadedPolygonUnion';
-export { default as PointGeometryUnion } from './union/PointGeometryUnion';
-export { default as UnaryUnionOp } from './union/UnaryUnionOp';
-export { default as UnionInteracting } from './union/UnionInteracting';
-export { default as UnionOp } from './union/UnionOp';
+export { default as CascadedPolygonUnion } from './union/CascadedPolygonUnion'
+export { default as PointGeometryUnion } from './union/PointGeometryUnion'
+export { default as UnaryUnionOp } from './union/UnaryUnionOp'
+export { default as UnionInteracting } from './union/UnionInteracting'
+export { default as UnionOp } from './union/UnionOp'
