b002890271294076d4f1781280d0236cc279eadb
Denis <carriere.denis@gmail.com>
2017-11-23 11:05:58 -0800

    Refactor Distance

--- src/org/locationtech/jts/operation/distance/ConnectedElementPointFilter.js
+++ src/org/locationtech/jts/operation/distance/ConnectedElementPointFilter.js
@@ -1,27 +1,27 @@
-import LineString from '../../geom/LineString';
-import Point from '../../geom/Point';
-import Polygon from '../../geom/Polygon';
-import extend from '../../../../../extend';
-import ArrayList from '../../../../../java/util/ArrayList';
-import GeometryFilter from '../../geom/GeometryFilter';
-export default function ConnectedElementPointFilter() {
-	this._pts = null;
-	let pts = arguments[0];
-	this._pts = pts;
+import LineString from '../../geom/LineString'
+import Point from '../../geom/Point'
+import Polygon from '../../geom/Polygon'
+import ArrayList from '../../../../../java/util/ArrayList'
+import GeometryFilter from '../../geom/GeometryFilter'
+
+export default class ConnectedElementPointFilter {
+  constructor () {
+    this._pts = null
+    let pts = arguments[0]
+    this._pts = pts
+  }
+  filter (geom) {
+    if (geom instanceof Point || geom instanceof LineString || geom instanceof Polygon) this._pts.add(geom.getCoordinate())
+  }
+  interfaces_ () {
+    return [GeometryFilter]
+  }
+  getClass () {
+    return ConnectedElementPointFilter
+  }
+  static getCoordinates (geom) {
+    var pts = new ArrayList()
+    geom.apply(new ConnectedElementPointFilter(pts))
+    return pts
+  }
 }
-extend(ConnectedElementPointFilter.prototype, {
-	filter: function (geom) {
-		if (geom instanceof Point || geom instanceof LineString || geom instanceof Polygon) this._pts.add(geom.getCoordinate());
-	},
-	interfaces_: function () {
-		return [GeometryFilter];
-	},
-	getClass: function () {
-		return ConnectedElementPointFilter;
-	}
-});
-ConnectedElementPointFilter.getCoordinates = function (geom) {
-	var pts = new ArrayList();
-	geom.apply(new ConnectedElementPointFilter(pts));
-	return pts;
-};
--- src/org/locationtech/jts/operation/distance/GeometryLocation.js
+++ src/org/locationtech/jts/operation/distance/GeometryLocation.js
@@ -1,36 +1,38 @@
-import extend from '../../../../../extend';
-export default function GeometryLocation() {
-	this._component = null;
-	this._segIndex = null;
-	this._pt = null;
-	if (arguments.length === 2) {
-		let component = arguments[0], pt = arguments[1];
-		GeometryLocation.call(this, component, GeometryLocation.INSIDE_AREA, pt);
-	} else if (arguments.length === 3) {
-		let component = arguments[0], segIndex = arguments[1], pt = arguments[2];
-		this._component = component;
-		this._segIndex = segIndex;
-		this._pt = pt;
-	}
+export default class GeometryLocation {
+  constructor () {
+    this._component = null
+    this._segIndex = null
+    this._pt = null
+    if (arguments.length === 2) {
+      const component = arguments[0]
+      const pt = arguments[1]
+      GeometryLocation.call(this, component, GeometryLocation.INSIDE_AREA, pt)
+    } else if (arguments.length === 3) {
+      const component = arguments[0]
+      const segIndex = arguments[1]
+      const pt = arguments[2]
+      this._component = component
+      this._segIndex = segIndex
+      this._pt = pt
+    }
+  }
+  isInsideArea () {
+    return this._segIndex === GeometryLocation.INSIDE_AREA
+  }
+  getCoordinate () {
+    return this._pt
+  }
+  getGeometryComponent () {
+    return this._component
+  }
+  getSegmentIndex () {
+    return this._segIndex
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return GeometryLocation
+  }
+  static get INSIDE_AREA () { return -1 }
 }
-extend(GeometryLocation.prototype, {
-	isInsideArea: function () {
-		return this._segIndex === GeometryLocation.INSIDE_AREA;
-	},
-	getCoordinate: function () {
-		return this._pt;
-	},
-	getGeometryComponent: function () {
-		return this._component;
-	},
-	getSegmentIndex: function () {
-		return this._segIndex;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return GeometryLocation;
-	}
-});
-GeometryLocation.INSIDE_AREA = -1;
