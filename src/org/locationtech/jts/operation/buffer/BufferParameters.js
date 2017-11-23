8bd0e262e4cf229935d481eef9cff7af01aaf625
Denis <carriere.denis@gmail.com>
2017-11-22 19:55:38 -0800

    Finish Buffer Op

--- src/org/locationtech/jts/operation/buffer/BufferParameters.js
+++ src/org/locationtech/jts/operation/buffer/BufferParameters.js
@@ -1,91 +1,95 @@
-import extend from '../../../../../extend';
-export default function BufferParameters() {
-	this._quadrantSegments = BufferParameters.DEFAULT_QUADRANT_SEGMENTS;
-	this._endCapStyle = BufferParameters.CAP_ROUND;
-	this._joinStyle = BufferParameters.JOIN_ROUND;
-	this._mitreLimit = BufferParameters.DEFAULT_MITRE_LIMIT;
-	this._isSingleSided = false;
-	this._simplifyFactor = BufferParameters.DEFAULT_SIMPLIFY_FACTOR;
-	if (arguments.length === 0) {} else if (arguments.length === 1) {
-		let quadrantSegments = arguments[0];
-		this.setQuadrantSegments(quadrantSegments);
-	} else if (arguments.length === 2) {
-		let quadrantSegments = arguments[0], endCapStyle = arguments[1];
-		this.setQuadrantSegments(quadrantSegments);
-		this.setEndCapStyle(endCapStyle);
-	} else if (arguments.length === 4) {
-		let quadrantSegments = arguments[0], endCapStyle = arguments[1], joinStyle = arguments[2], mitreLimit = arguments[3];
-		this.setQuadrantSegments(quadrantSegments);
-		this.setEndCapStyle(endCapStyle);
-		this.setJoinStyle(joinStyle);
-		this.setMitreLimit(mitreLimit);
-	}
+export default class BufferParameters {
+  constructor () {
+    this._quadrantSegments = BufferParameters.DEFAULT_QUADRANT_SEGMENTS
+    this._endCapStyle = BufferParameters.CAP_ROUND
+    this._joinStyle = BufferParameters.JOIN_ROUND
+    this._mitreLimit = BufferParameters.DEFAULT_MITRE_LIMIT
+    this._isSingleSided = false
+    this._simplifyFactor = BufferParameters.DEFAULT_SIMPLIFY_FACTOR
+
+    if (arguments.length === 0) {} else if (arguments.length === 1) {
+      const quadrantSegments = arguments[0]
+      this.setQuadrantSegments(quadrantSegments)
+    } else if (arguments.length === 2) {
+      const quadrantSegments = arguments[0]
+      const endCapStyle = arguments[1]
+      this.setQuadrantSegments(quadrantSegments)
+      this.setEndCapStyle(endCapStyle)
+    } else if (arguments.length === 4) {
+      const quadrantSegments = arguments[0]
+      const endCapStyle = arguments[1]
+      const joinStyle = arguments[2]
+      const mitreLimit = arguments[3]
+      this.setQuadrantSegments(quadrantSegments)
+      this.setEndCapStyle(endCapStyle)
+      this.setJoinStyle(joinStyle)
+      this.setMitreLimit(mitreLimit)
+    }
+  }
+  getEndCapStyle () {
+    return this._endCapStyle
+  }
+  isSingleSided () {
+    return this._isSingleSided
+  }
+  setQuadrantSegments (quadSegs) {
+    this._quadrantSegments = quadSegs
+    if (this._quadrantSegments === 0) this._joinStyle = BufferParameters.JOIN_BEVEL
+    if (this._quadrantSegments < 0) {
+      this._joinStyle = BufferParameters.JOIN_MITRE
+      this._mitreLimit = Math.abs(this._quadrantSegments)
+    }
+    if (quadSegs <= 0) {
+      this._quadrantSegments = 1
+    }
+    if (this._joinStyle !== BufferParameters.JOIN_ROUND) {
+      this._quadrantSegments = BufferParameters.DEFAULT_QUADRANT_SEGMENTS
+    }
+  }
+  getJoinStyle () {
+    return this._joinStyle
+  }
+  setJoinStyle (joinStyle) {
+    this._joinStyle = joinStyle
+  }
+  setSimplifyFactor (simplifyFactor) {
+    this._simplifyFactor = simplifyFactor < 0 ? 0 : simplifyFactor
+  }
+  getSimplifyFactor () {
+    return this._simplifyFactor
+  }
+  getQuadrantSegments () {
+    return this._quadrantSegments
+  }
+  setEndCapStyle (endCapStyle) {
+    this._endCapStyle = endCapStyle
+  }
+  getMitreLimit () {
+    return this._mitreLimit
+  }
+  setMitreLimit (mitreLimit) {
+    this._mitreLimit = mitreLimit
+  }
+  setSingleSided (isSingleSided) {
+    this._isSingleSided = isSingleSided
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return BufferParameters
+  }
+  static bufferDistanceError (quadSegs) {
+    var alpha = Math.PI / 2.0 / quadSegs
+    return 1 - Math.cos(alpha / 2.0)
+  }
+  static get CAP_ROUND () { return 1 }
+  static get CAP_FLAT () { return 2 }
+  static get CAP_SQUARE () { return 3 }
+  static get JOIN_ROUND () { return 1 }
+  static get JOIN_MITRE () { return 2 }
+  static get JOIN_BEVEL () { return 3 }
+  static get DEFAULT_QUADRANT_SEGMENTS () { return 8 }
+  static get DEFAULT_MITRE_LIMIT () { return 5.0 }
+  static get DEFAULT_SIMPLIFY_FACTOR () { return 0.01 }
 }
-extend(BufferParameters.prototype, {
-	getEndCapStyle: function () {
-		return this._endCapStyle;
-	},
-	isSingleSided: function () {
-		return this._isSingleSided;
-	},
-	setQuadrantSegments: function (quadSegs) {
-		this._quadrantSegments = quadSegs;
-		if (this._quadrantSegments === 0) this._joinStyle = BufferParameters.JOIN_BEVEL;
-		if (this._quadrantSegments < 0) {
-			this._joinStyle = BufferParameters.JOIN_MITRE;
-			this._mitreLimit = Math.abs(this._quadrantSegments);
-		}
-		if (quadSegs <= 0) {
-			this._quadrantSegments = 1;
-		}
-		if (this._joinStyle !== BufferParameters.JOIN_ROUND) {
-			this._quadrantSegments = BufferParameters.DEFAULT_QUADRANT_SEGMENTS;
-		}
-	},
-	getJoinStyle: function () {
-		return this._joinStyle;
-	},
-	setJoinStyle: function (joinStyle) {
-		this._joinStyle = joinStyle;
-	},
-	setSimplifyFactor: function (simplifyFactor) {
-		this._simplifyFactor = simplifyFactor < 0 ? 0 : simplifyFactor;
-	},
-	getSimplifyFactor: function () {
-		return this._simplifyFactor;
-	},
-	getQuadrantSegments: function () {
-		return this._quadrantSegments;
-	},
-	setEndCapStyle: function (endCapStyle) {
-		this._endCapStyle = endCapStyle;
-	},
-	getMitreLimit: function () {
-		return this._mitreLimit;
-	},
-	setMitreLimit: function (mitreLimit) {
-		this._mitreLimit = mitreLimit;
-	},
-	setSingleSided: function (isSingleSided) {
-		this._isSingleSided = isSingleSided;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return BufferParameters;
-	}
-});
-BufferParameters.bufferDistanceError = function (quadSegs) {
-	var alpha = Math.PI / 2.0 / quadSegs;
-	return 1 - Math.cos(alpha / 2.0);
-};
-BufferParameters.CAP_ROUND = 1;
-BufferParameters.CAP_FLAT = 2;
-BufferParameters.CAP_SQUARE = 3;
-BufferParameters.JOIN_ROUND = 1;
-BufferParameters.JOIN_MITRE = 2;
-BufferParameters.JOIN_BEVEL = 3;
-BufferParameters.DEFAULT_QUADRANT_SEGMENTS = 8;
-BufferParameters.DEFAULT_MITRE_LIMIT = 5.0;
-BufferParameters.DEFAULT_SIMPLIFY_FACTOR = 0.01;
