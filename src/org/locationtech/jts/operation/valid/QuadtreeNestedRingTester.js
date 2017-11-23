047438614481491893fc30d390a2e24bfce5850e
Denis <carriere.denis@gmail.com>
2017-11-23 12:30:49 -0800

    Refactor exports

--- src/org/locationtech/jts/operation/valid/QuadtreeNestedRingTester.js
+++ src/org/locationtech/jts/operation/valid/QuadtreeNestedRingTester.js
@@ -1,61 +1,61 @@
-import Quadtree from '../../index/quadtree/Quadtree';
-import CGAlgorithms from '../../algorithm/CGAlgorithms';
-import IsValidOp from './IsValidOp';
-import extend from '../../../../../extend';
-import ArrayList from '../../../../../java/util/ArrayList';
-import Envelope from '../../geom/Envelope';
-import Assert from '../../util/Assert';
-export default function QuadtreeNestedRingTester() {
-	this._graph = null;
-	this._rings = new ArrayList();
-	this._totalEnv = new Envelope();
-	this._quadtree = null;
-	this._nestedPt = null;
-	let graph = arguments[0];
-	this._graph = graph;
+import Quadtree from '../../index/quadtree/Quadtree'
+import CGAlgorithms from '../../algorithm/CGAlgorithms'
+import IsValidOp from './IsValidOp'
+import ArrayList from '../../../../../java/util/ArrayList'
+import Envelope from '../../geom/Envelope'
+import Assert from '../../util/Assert'
+
+export default class QuadtreeNestedRingTester {
+  constructor () {
+    this._graph = null
+    this._rings = new ArrayList()
+    this._totalEnv = new Envelope()
+    this._quadtree = null
+    this._nestedPt = null
+    let graph = arguments[0]
+    this._graph = graph
+  }
+  getNestedPoint () {
+    return this._nestedPt
+  }
+  isNonNested () {
+    this.buildQuadtree()
+    for (let i = 0; i < this._rings.size(); i++) {
+      const innerRing = this._rings.get(i)
+      const innerRingPts = innerRing.getCoordinates()
+      const results = this._quadtree.query(innerRing.getEnvelopeInternal())
+      for (let j = 0; j < results.size(); j++) {
+        const searchRing = results.get(j)
+        const searchRingPts = searchRing.getCoordinates()
+        if (innerRing === searchRing) continue
+        if (!innerRing.getEnvelopeInternal().intersects(searchRing.getEnvelopeInternal())) continue
+        const innerRingPt = IsValidOp.findPtNotNode(innerRingPts, searchRing, this._graph)
+        Assert.isTrue(innerRingPt !== null, 'Unable to find a ring point not a node of the search ring')
+        const isInside = CGAlgorithms.isPointInRing(innerRingPt, searchRingPts)
+        if (isInside) {
+          this._nestedPt = innerRingPt
+          return false
+        }
+      }
+    }
+    return true
+  }
+  add (ring) {
+    this._rings.add(ring)
+    this._totalEnv.expandToInclude(ring.getEnvelopeInternal())
+  }
+  buildQuadtree () {
+    this._quadtree = new Quadtree()
+    for (let i = 0; i < this._rings.size(); i++) {
+      const ring = this._rings.get(i)
+      const env = ring.getEnvelopeInternal()
+      this._quadtree.insert(env, ring)
+    }
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return QuadtreeNestedRingTester
+  }
 }
-extend(QuadtreeNestedRingTester.prototype, {
-	getNestedPoint: function () {
-		return this._nestedPt;
-	},
-	isNonNested: function () {
-		this.buildQuadtree();
-		for (var i = 0; i < this._rings.size(); i++) {
-			var innerRing = this._rings.get(i);
-			var innerRingPts = innerRing.getCoordinates();
-			var results = this._quadtree.query(innerRing.getEnvelopeInternal());
-			for (var j = 0; j < results.size(); j++) {
-				var searchRing = results.get(j);
-				var searchRingPts = searchRing.getCoordinates();
-				if (innerRing === searchRing) continue;
-				if (!innerRing.getEnvelopeInternal().intersects(searchRing.getEnvelopeInternal())) continue;
-				var innerRingPt = IsValidOp.findPtNotNode(innerRingPts, searchRing, this._graph);
-				Assert.isTrue(innerRingPt !== null, "Unable to find a ring point not a node of the search ring");
-				var isInside = CGAlgorithms.isPointInRing(innerRingPt, searchRingPts);
-				if (isInside) {
-					this._nestedPt = innerRingPt;
-					return false;
-				}
-			}
-		}
-		return true;
-	},
-	add: function (ring) {
-		this._rings.add(ring);
-		this._totalEnv.expandToInclude(ring.getEnvelopeInternal());
-	},
-	buildQuadtree: function () {
-		this._quadtree = new Quadtree();
-		for (var i = 0; i < this._rings.size(); i++) {
-			var ring = this._rings.get(i);
-			var env = ring.getEnvelopeInternal();
-			this._quadtree.insert(env, ring);
-		}
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return QuadtreeNestedRingTester;
-	}
-});
--- src/org/locationtech/jts/operation/valid/TopologyValidationError.js
+++ src/org/locationtech/jts/operation/valid/TopologyValidationError.js
@@ -1,48 +1,48 @@
-import extend from '../../../../../extend';
-export default function TopologyValidationError() {
-	this._errorType = null;
-	this._pt = null;
-	if (arguments.length === 1) {
-		let errorType = arguments[0];
-		TopologyValidationError.call(this, errorType, null);
-	} else if (arguments.length === 2) {
-		let errorType = arguments[0], pt = arguments[1];
-		this._errorType = errorType;
-		if (pt !== null) this._pt = pt.copy();
-	}
+export default class TopologyValidationError {
+  constructor () {
+    this._errorType = null
+    this._pt = null
+    if (arguments.length === 1) {
+      const errorType = arguments[0]
+      TopologyValidationError.call(this, errorType, null)
+    } else if (arguments.length === 2) {
+      const errorType = arguments[0]
+      const pt = arguments[1]
+      this._errorType = errorType
+      if (pt !== null) this._pt = pt.copy()
+    }
+  }
+  getErrorType () {
+    return this._errorType
+  }
+  getMessage () {
+    return TopologyValidationError.errMsg[this._errorType]
+  }
+  getCoordinate () {
+    return this._pt
+  }
+  toString () {
+    let locStr = ''
+    if (this._pt !== null) locStr = ' at or near point ' + this._pt
+    return this.getMessage() + locStr
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return TopologyValidationError
+  }
+  static get ERROR () { return 0 }
+  static get REPEATED_POINT () { return 1 }
+  static get HOLE_OUTSIDE_SHELL () { return 2 }
+  static get NESTED_HOLES () { return 3 }
+  static get DISCONNECTED_INTERIOR () { return 4 }
+  static get SELF_INTERSECTION () { return 5 }
+  static get RING_SELF_INTERSECTION () { return 6 }
+  static get NESTED_SHELLS () { return 7 }
+  static get DUPLICATE_RINGS () { return 8 }
+  static get TOO_FEW_POINTS () { return 9 }
+  static get INVALID_COORDINATE () { return 10 }
+  static get RING_NOT_CLOSED () { return 11 }
+  static get errMsg () { return ['Topology Validation Error', 'Repeated Point', 'Hole lies outside shell', 'Holes are nested', 'Interior is disconnected', 'Self-intersection', 'Ring Self-intersection', 'Nested shells', 'Duplicate Rings', 'Too few distinct points in geometry component', 'Invalid Coordinate', 'Ring is not closed'] }
 }
-extend(TopologyValidationError.prototype, {
-	getErrorType: function () {
-		return this._errorType;
-	},
-	getMessage: function () {
-		return TopologyValidationError.errMsg[this._errorType];
-	},
-	getCoordinate: function () {
-		return this._pt;
-	},
-	toString: function () {
-		var locStr = "";
-		if (this._pt !== null) locStr = " at or near point " + this._pt;
-		return this.getMessage() + locStr;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return TopologyValidationError;
-	}
-});
-TopologyValidationError.ERROR = 0;
-TopologyValidationError.REPEATED_POINT = 1;
-TopologyValidationError.HOLE_OUTSIDE_SHELL = 2;
-TopologyValidationError.NESTED_HOLES = 3;
-TopologyValidationError.DISCONNECTED_INTERIOR = 4;
-TopologyValidationError.SELF_INTERSECTION = 5;
-TopologyValidationError.RING_SELF_INTERSECTION = 6;
-TopologyValidationError.NESTED_SHELLS = 7;
-TopologyValidationError.DUPLICATE_RINGS = 8;
-TopologyValidationError.TOO_FEW_POINTS = 9;
-TopologyValidationError.INVALID_COORDINATE = 10;
-TopologyValidationError.RING_NOT_CLOSED = 11;
-TopologyValidationError.errMsg = ["Topology Validation Error", "Repeated Point", "Hole lies outside shell", "Holes are nested", "Interior is disconnected", "Self-intersection", "Ring Self-intersection", "Nested shells", "Duplicate Rings", "Too few distinct points in geometry component", "Invalid Coordinate", "Ring is not closed"];
