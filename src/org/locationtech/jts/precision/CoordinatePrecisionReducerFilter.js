5ca628228d6c1fee36b203e3bdfdb2d928870d64
Denis <carriere.denis@gmail.com>
2017-11-25 00:35:08 -0800

    Refactor precision

--- src/org/locationtech/jts/precision/CoordinatePrecisionReducerFilter.js
+++ src/org/locationtech/jts/precision/CoordinatePrecisionReducerFilter.js
@@ -1,25 +1,25 @@
-import extend from '../../../../extend';
-import CoordinateSequenceFilter from '../geom/CoordinateSequenceFilter';
-export default function CoordinatePrecisionReducerFilter() {
-	this._precModel = null;
-	let precModel = arguments[0];
-	this._precModel = precModel;
+import CoordinateSequenceFilter from '../geom/CoordinateSequenceFilter'
+
+export default class CoordinatePrecisionReducerFilter {
+  constructor () {
+    this._precModel = null
+    let precModel = arguments[0]
+    this._precModel = precModel
+  }
+  filter (seq, i) {
+    seq.setOrdinate(i, 0, this._precModel.makePrecise(seq.getOrdinate(i, 0)))
+    seq.setOrdinate(i, 1, this._precModel.makePrecise(seq.getOrdinate(i, 1)))
+  }
+  isDone () {
+    return false
+  }
+  isGeometryChanged () {
+    return true
+  }
+  interfaces_ () {
+    return [CoordinateSequenceFilter]
+  }
+  getClass () {
+    return CoordinatePrecisionReducerFilter
+  }
 }
-extend(CoordinatePrecisionReducerFilter.prototype, {
-	filter: function (seq, i) {
-		seq.setOrdinate(i, 0, this._precModel.makePrecise(seq.getOrdinate(i, 0)));
-		seq.setOrdinate(i, 1, this._precModel.makePrecise(seq.getOrdinate(i, 1)));
-	},
-	isDone: function () {
-		return false;
-	},
-	isGeometryChanged: function () {
-		return true;
-	},
-	interfaces_: function () {
-		return [CoordinateSequenceFilter];
-	},
-	getClass: function () {
-		return CoordinatePrecisionReducerFilter;
-	}
-});
--- src/org/locationtech/jts/precision/GeometryPrecisionReducer.js
+++ src/org/locationtech/jts/precision/GeometryPrecisionReducer.js
@@ -12,44 +12,44 @@ export default class GeometryPrecisionReducer {
     this._targetPM = pm || null
   }
   fixPolygonalTopology (geom) {
-    var geomToBuffer = geom
+    let geomToBuffer = geom
     if (!this._changePrecisionModel) {
       geomToBuffer = this.changePM(geom, this._targetPM)
     }
-    var bufGeom = geomToBuffer.buffer(0)
-    var finalGeom = bufGeom
+    const bufGeom = geomToBuffer.buffer(0)
+    let finalGeom = bufGeom
     if (!this._changePrecisionModel) {
       finalGeom = this.changePM(bufGeom, geom.getPrecisionModel())
     }
     return finalGeom
   }
   reducePointwise (geom) {
-    var geomEdit = null
+    let geomEdit = null
     if (this._changePrecisionModel) {
-      var newFactory = this.createFactory(geom.getFactory(), this._targetPM)
+      const newFactory = this.createFactory(geom.getFactory(), this._targetPM)
       geomEdit = new GeometryEditor(newFactory)
     } else geomEdit = new GeometryEditor()
-    var finalRemoveCollapsed = this._removeCollapsed
+    let finalRemoveCollapsed = this._removeCollapsed
     if (geom.getDimension() >= 2) finalRemoveCollapsed = true
-    var reduceGeom = geomEdit.edit(geom, new PrecisionReducerCoordinateOperation(this._targetPM, finalRemoveCollapsed))
+    const reduceGeom = geomEdit.edit(geom, new PrecisionReducerCoordinateOperation(this._targetPM, finalRemoveCollapsed))
     return reduceGeom
   }
   changePM (geom, newPM) {
-    var geomEditor = this.createEditor(geom.getFactory(), newPM)
+    const geomEditor = this.createEditor(geom.getFactory(), newPM)
     return geomEditor.edit(geom, new GeometryEditor.NoOpGeometryOperation())
   }
   setRemoveCollapsedComponents (removeCollapsed) {
     this._removeCollapsed = removeCollapsed
   }
   createFactory (inputFactory, pm) {
-    var newFactory = new GeometryFactory(pm, inputFactory.getSRID(), inputFactory.getCoordinateSequenceFactory())
+    const newFactory = new GeometryFactory(pm, inputFactory.getSRID(), inputFactory.getCoordinateSequenceFactory())
     return newFactory
   }
   setChangePrecisionModel (changePrecisionModel) {
     this._changePrecisionModel = changePrecisionModel
   }
   reduce (geom) {
-    var reducePW = this.reducePointwise(geom)
+    const reducePW = this.reducePointwise(geom)
     if (this._isPointwise) return reducePW
     if (!hasInterface(reducePW, Polygonal)) return reducePW
     if (reducePW.isValid()) return reducePW
@@ -60,8 +60,8 @@ export default class GeometryPrecisionReducer {
   }
   createEditor (geomFactory, newPM) {
     if (geomFactory.getPrecisionModel() === newPM) return new GeometryEditor()
-    var newFactory = this.createFactory(geomFactory, newPM)
-    var geomEdit = new GeometryEditor(newFactory)
+    const newFactory = this.createFactory(geomFactory, newPM)
+    const geomEdit = new GeometryEditor(newFactory)
     return geomEdit
   }
   interfaces_ () {
@@ -71,11 +71,11 @@ export default class GeometryPrecisionReducer {
     return GeometryPrecisionReducer
   }
   static reduce (g, precModel) {
-    var reducer = new GeometryPrecisionReducer(precModel)
+    const reducer = new GeometryPrecisionReducer(precModel)
     return reducer.reduce(g)
   }
   static reducePointwise (g, precModel) {
-    var reducer = new GeometryPrecisionReducer(precModel)
+    const reducer = new GeometryPrecisionReducer(precModel)
     reducer.setPointwise(true)
     return reducer.reduce(g)
   }
