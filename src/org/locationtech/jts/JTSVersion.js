ddfa02a97be42c0dbdcc064be5b008f5f5958a15
Denis <carriere.denis@gmail.com>
2017-11-25 02:09:28 -0800

    lint

--- src/org/locationtech/jts/JTSVersion.js
+++ src/org/locationtech/jts/JTSVersion.js
@@ -1,33 +1,32 @@
-import extend from '../../../extend';
-import System from '../../../java/lang/System';
-export default function JTSVersion() {}
-extend(JTSVersion.prototype, {
-	getMajor: function () {
-		return JTSVersion.MAJOR;
-	},
-	getPatch: function () {
-		return JTSVersion.PATCH;
-	},
-	getMinor: function () {
-		return JTSVersion.MINOR;
-	},
-	toString: function () {
-		var ver = JTSVersion.MAJOR + "." + JTSVersion.MINOR + "." + JTSVersion.PATCH;
-		if (JTSVersion.releaseInfo !== null && JTSVersion.releaseInfo.length > 0) return ver + " " + JTSVersion.releaseInfo;
-		return ver;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return JTSVersion;
-	}
-});
-JTSVersion.main = function (args) {
-	System.out.println(JTSVersion.CURRENT_VERSION);
-};
-JTSVersion.CURRENT_VERSION = new JTSVersion();
-JTSVersion.MAJOR = 1;
-JTSVersion.MINOR = 14;
-JTSVersion.PATCH = 0;
-JTSVersion.releaseInfo = "";
+import System from '../../../java/lang/System'
+
+export default class JTSVersion {
+  getMajor () {
+    return JTSVersion.MAJOR
+  }
+  getPatch () {
+    return JTSVersion.PATCH
+  }
+  getMinor () {
+    return JTSVersion.MINOR
+  }
+  toString () {
+    var ver = JTSVersion.MAJOR + '.' + JTSVersion.MINOR + '.' + JTSVersion.PATCH
+    if (JTSVersion.releaseInfo !== null && JTSVersion.releaseInfo.length > 0) return ver + ' ' + JTSVersion.releaseInfo
+    return ver
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return JTSVersion
+  }
+  static main (args) {
+    System.out.println(JTSVersion.CURRENT_VERSION)
+  }
+  static get CURRENT_VERSION () { return new JTSVersion() }
+  static get MAJOR () { return 1 }
+  static get MINOR () { return 14 }
+  static get PATCH () { return 0 }
+  static get releaseInfo () { return '' }
+}
