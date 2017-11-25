e78785d3b1b245419df191835cd8214a6e0c455a
Denis <carriere.denis@gmail.com>
2017-11-24 22:31:49 -0800

    Refactor Math

--- src/org/locationtech/jts/math/Plane3D.js
+++ src/org/locationtech/jts/math/Plane3D.js
@@ -1,40 +1,41 @@
-import IllegalArgumentException from '../../../../java/lang/IllegalArgumentException';
-import Double from '../../../../java/lang/Double';
-import extend from '../../../../extend';
-import Vector3D from './Vector3D';
-export default function Plane3D() {
-	this._normal = null;
-	this._basePt = null;
-	let normal = arguments[0], basePt = arguments[1];
-	this._normal = normal;
-	this._basePt = basePt;
+import IllegalArgumentException from '../../../../java/lang/IllegalArgumentException'
+import Double from '../../../../java/lang/Double'
+import Vector3D from './Vector3D'
+
+export default class Plane3D {
+  contructor () {
+    this._normal = null
+    this._basePt = null
+    const normal = arguments[0]
+    const basePt = arguments[1]
+    this._normal = normal
+    this._basePt = basePt
+  }
+  closestAxisPlane () {
+    const xmag = Math.abs(this._normal.getX())
+    const ymag = Math.abs(this._normal.getY())
+    const zmag = Math.abs(this._normal.getZ())
+    if (xmag > ymag) {
+      if (xmag > zmag) return Plane3D.YZ_PLANE; else return Plane3D.XY_PLANE
+    } else if (zmag > ymag) {
+      return Plane3D.XY_PLANE
+    }
+    return Plane3D.XZ_PLANE
+  }
+  orientedDistance (p) {
+    const pb = new Vector3D(p, this._basePt)
+    const pbdDotNormal = pb.dot(this._normal)
+    if (Double.isNaN(pbdDotNormal)) throw new IllegalArgumentException('3D Coordinate has NaN ordinate')
+    const d = pbdDotNormal / this._normal.length()
+    return d
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return Plane3D
+  }
+  static get XY_PLANE () { return 1 }
+  static get YZ_PLANE () { return 2 }
+  static get XZ_PLANE () { return 3 }
 }
-extend(Plane3D.prototype, {
-	closestAxisPlane: function () {
-		var xmag = Math.abs(this._normal.getX());
-		var ymag = Math.abs(this._normal.getY());
-		var zmag = Math.abs(this._normal.getZ());
-		if (xmag > ymag) {
-			if (xmag > zmag) return Plane3D.YZ_PLANE; else return Plane3D.XY_PLANE;
-		} else if (zmag > ymag) {
-			return Plane3D.XY_PLANE;
-		}
-		return Plane3D.XZ_PLANE;
-	},
-	orientedDistance: function (p) {
-		var pb = new Vector3D(p, this._basePt);
-		var pbdDotNormal = pb.dot(this._normal);
-		if (Double.isNaN(pbdDotNormal)) throw new IllegalArgumentException("3D Coordinate has NaN ordinate");
-		var d = pbdDotNormal / this._normal.length();
-		return d;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return Plane3D;
-	}
-});
-Plane3D.XY_PLANE = 1;
-Plane3D.YZ_PLANE = 2;
-Plane3D.XZ_PLANE = 3;
