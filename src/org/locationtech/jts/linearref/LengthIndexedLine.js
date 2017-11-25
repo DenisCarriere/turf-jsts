27dad4025fd6ed6fb3310a9ffc6b76690a838b8f
Denis <carriere.denis@gmail.com>
2017-11-24 22:49:50 -0800

    Refactor lineareaf

--- src/org/locationtech/jts/linearref/LengthIndexedLine.js
+++ src/org/locationtech/jts/linearref/LengthIndexedLine.js
@@ -1,84 +1,86 @@
-import LocationIndexedLine from './LocationIndexedLine';
-import extend from '../../../../extend';
-import LengthIndexOfPoint from './LengthIndexOfPoint';
-import LocationIndexOfLine from './LocationIndexOfLine';
-import LengthLocationMap from './LengthLocationMap';
-import ExtractLineByLocation from './ExtractLineByLocation';
-export default function LengthIndexedLine() {
-	this._linearGeom = null;
-	let linearGeom = arguments[0];
-	this._linearGeom = linearGeom;
+import LocationIndexedLine from './LocationIndexedLine'
+import LengthIndexOfPoint from './LengthIndexOfPoint'
+import LocationIndexOfLine from './LocationIndexOfLine'
+import LengthLocationMap from './LengthLocationMap'
+import ExtractLineByLocation from './ExtractLineByLocation'
+
+export default class LengthIndexedLine {
+  constructor () {
+    this._linearGeom = null
+    const linearGeom = arguments[0]
+    this._linearGeom = linearGeom
+  }
+  clampIndex (index) {
+    const posIndex = this.positiveIndex(index)
+    const startIndex = this.getStartIndex()
+    if (posIndex < startIndex) return startIndex
+    const endIndex = this.getEndIndex()
+    if (posIndex > endIndex) return endIndex
+    return posIndex
+  }
+  locationOf () {
+    if (arguments.length === 1) {
+      const index = arguments[0]
+      return LengthLocationMap.getLocation(this._linearGeom, index)
+    } else if (arguments.length === 2) {
+      const index = arguments[0]
+      const resolveLower = arguments[1]
+      return LengthLocationMap.getLocation(this._linearGeom, index, resolveLower)
+    }
+  }
+  project (pt) {
+    return LengthIndexOfPoint.indexOf(this._linearGeom, pt)
+  }
+  positiveIndex (index) {
+    if (index >= 0.0) return index
+    return this._linearGeom.getLength() + index
+  }
+  extractPoint () {
+    if (arguments.length === 1) {
+      const index = arguments[0]
+      const loc = LengthLocationMap.getLocation(this._linearGeom, index)
+      return loc.getCoordinate(this._linearGeom)
+    } else if (arguments.length === 2) {
+      const index = arguments[0]
+      const offsetDistance = arguments[1]
+      const loc = LengthLocationMap.getLocation(this._linearGeom, index)
+      const locLow = loc.toLowest(this._linearGeom)
+      return locLow.getSegment(this._linearGeom).pointAlongOffset(locLow.getSegmentFraction(), offsetDistance)
+    }
+  }
+  isValidIndex (index) {
+    return index >= this.getStartIndex() && index <= this.getEndIndex()
+  }
+  getEndIndex () {
+    return this._linearGeom.getLength()
+  }
+  getStartIndex () {
+    return 0.0
+  }
+  indexOfAfter (pt, minIndex) {
+    return LengthIndexOfPoint.indexOfAfter(this._linearGeom, pt, minIndex)
+  }
+  extractLine (startIndex, endIndex) {
+    const lil = new LocationIndexedLine(this._linearGeom)
+    const startIndex2 = this.clampIndex(startIndex)
+    const endIndex2 = this.clampIndex(endIndex)
+    const resolveStartLower = startIndex2 === endIndex2
+    const startLoc = this.locationOf(startIndex2, resolveStartLower)
+    const endLoc = this.locationOf(endIndex2)
+    return ExtractLineByLocation.extract(this._linearGeom, startLoc, endLoc)
+  }
+  indexOf (pt) {
+    return LengthIndexOfPoint.indexOf(this._linearGeom, pt)
+  }
+  indicesOf (subLine) {
+    const locIndex = LocationIndexOfLine.indicesOf(this._linearGeom, subLine)
+    const index = [LengthLocationMap.getLength(this._linearGeom, locIndex[0]), LengthLocationMap.getLength(this._linearGeom, locIndex[1])]
+    return index
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return LengthIndexedLine
+  }
 }
-extend(LengthIndexedLine.prototype, {
-	clampIndex: function (index) {
-		var posIndex = this.positiveIndex(index);
-		var startIndex = this.getStartIndex();
-		if (posIndex < startIndex) return startIndex;
-		var endIndex = this.getEndIndex();
-		if (posIndex > endIndex) return endIndex;
-		return posIndex;
-	},
-	locationOf: function () {
-		if (arguments.length === 1) {
-			let index = arguments[0];
-			return LengthLocationMap.getLocation(this._linearGeom, index);
-		} else if (arguments.length === 2) {
-			let index = arguments[0], resolveLower = arguments[1];
-			return LengthLocationMap.getLocation(this._linearGeom, index, resolveLower);
-		}
-	},
-	project: function (pt) {
-		return LengthIndexOfPoint.indexOf(this._linearGeom, pt);
-	},
-	positiveIndex: function (index) {
-		if (index >= 0.0) return index;
-		return this._linearGeom.getLength() + index;
-	},
-	extractPoint: function () {
-		if (arguments.length === 1) {
-			let index = arguments[0];
-			var loc = LengthLocationMap.getLocation(this._linearGeom, index);
-			return loc.getCoordinate(this._linearGeom);
-		} else if (arguments.length === 2) {
-			let index = arguments[0], offsetDistance = arguments[1];
-			var loc = LengthLocationMap.getLocation(this._linearGeom, index);
-			var locLow = loc.toLowest(this._linearGeom);
-			return locLow.getSegment(this._linearGeom).pointAlongOffset(locLow.getSegmentFraction(), offsetDistance);
-		}
-	},
-	isValidIndex: function (index) {
-		return index >= this.getStartIndex() && index <= this.getEndIndex();
-	},
-	getEndIndex: function () {
-		return this._linearGeom.getLength();
-	},
-	getStartIndex: function () {
-		return 0.0;
-	},
-	indexOfAfter: function (pt, minIndex) {
-		return LengthIndexOfPoint.indexOfAfter(this._linearGeom, pt, minIndex);
-	},
-	extractLine: function (startIndex, endIndex) {
-		var lil = new LocationIndexedLine(this._linearGeom);
-		var startIndex2 = this.clampIndex(startIndex);
-		var endIndex2 = this.clampIndex(endIndex);
-		var resolveStartLower = startIndex2 === endIndex2;
-		var startLoc = this.locationOf(startIndex2, resolveStartLower);
-		var endLoc = this.locationOf(endIndex2);
-		return ExtractLineByLocation.extract(this._linearGeom, startLoc, endLoc);
-	},
-	indexOf: function (pt) {
-		return LengthIndexOfPoint.indexOf(this._linearGeom, pt);
-	},
-	indicesOf: function (subLine) {
-		var locIndex = LocationIndexOfLine.indicesOf(this._linearGeom, subLine);
-		var index = [LengthLocationMap.getLength(this._linearGeom, locIndex[0]), LengthLocationMap.getLength(this._linearGeom, locIndex[1])];
-		return index;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return LengthIndexedLine;
-	}
-});
--- src/org/locationtech/jts/linearref/LinearIterator.js
+++ src/org/locationtech/jts/linearref/LinearIterator.js
@@ -1,80 +1,88 @@
-import hasInterface from '../../../../hasInterface';
-import IllegalArgumentException from '../../../../java/lang/IllegalArgumentException';
-import extend from '../../../../extend';
-import Lineal from '../geom/Lineal';
-export default function LinearIterator() {
-	this._linearGeom = null;
-	this._numLines = null;
-	this._currentLine = null;
-	this._componentIndex = 0;
-	this._vertexIndex = 0;
-	if (arguments.length === 1) {
-		let linear = arguments[0];
-		LinearIterator.call(this, linear, 0, 0);
-	} else if (arguments.length === 2) {
-		let linear = arguments[0], start = arguments[1];
-		LinearIterator.call(this, linear, start.getComponentIndex(), LinearIterator.segmentEndVertexIndex(start));
-	} else if (arguments.length === 3) {
-		let linearGeom = arguments[0], componentIndex = arguments[1], vertexIndex = arguments[2];
-		if (!hasInterface(linearGeom, Lineal)) throw new IllegalArgumentException("Lineal geometry is required");
-		this._linearGeom = linearGeom;
-		this._numLines = linearGeom.getNumGeometries();
-		this._componentIndex = componentIndex;
-		this._vertexIndex = vertexIndex;
-		this.loadCurrentLine();
-	}
+import hasInterface from '../../../../hasInterface'
+import IllegalArgumentException from '../../../../java/lang/IllegalArgumentException'
+import Lineal from '../geom/Lineal'
+
+export default class LinearIterator {
+  constructor () {
+    this._linearGeom = null
+    this._numLines = null
+    this._currentLine = null
+    this._componentIndex = 0
+    this._vertexIndex = 0
+    let linearGeom
+    let componentIndex
+    let vertexIndex
+    if (arguments.length === 1) {
+      linearGeom = arguments[0]
+      componentIndex = 0
+      vertexIndex = 0
+    } else if (arguments.length === 2) {
+      linearGeom = arguments[0]
+      const start = arguments[1]
+      componentIndex = start.getComponentIndex()
+      vertexIndex = LinearIterator.segmentEndVertexIndex(start)
+    } else if (arguments.length === 3) {
+      linearGeom = arguments[0]
+      componentIndex = arguments[1]
+      vertexIndex = arguments[2]
+    }
+    if (!hasInterface(linearGeom, Lineal)) throw new IllegalArgumentException('Lineal geometry is required')
+    this._linearGeom = linearGeom
+    this._numLines = linearGeom.getNumGeometries()
+    this._componentIndex = componentIndex
+    this._vertexIndex = vertexIndex
+    this.loadCurrentLine()
+  }
+  getComponentIndex () {
+    return this._componentIndex
+  }
+  getLine () {
+    return this._currentLine
+  }
+  getVertexIndex () {
+    return this._vertexIndex
+  }
+  getSegmentEnd () {
+    if (this._vertexIndex < this.getLine().getNumPoints() - 1) return this._currentLine.getCoordinateN(this._vertexIndex + 1)
+    return null
+  }
+  next () {
+    if (!this.hasNext()) return null
+    this._vertexIndex++
+    if (this._vertexIndex >= this._currentLine.getNumPoints()) {
+      this._componentIndex++
+      this.loadCurrentLine()
+      this._vertexIndex = 0
+    }
+  }
+  loadCurrentLine () {
+    if (this._componentIndex >= this._numLines) {
+      this._currentLine = null
+      return null
+    }
+    this._currentLine = this._linearGeom.getGeometryN(this._componentIndex)
+  }
+  getSegmentStart () {
+    return this._currentLine.getCoordinateN(this._vertexIndex)
+  }
+  isEndOfLine () {
+    if (this._componentIndex >= this._numLines) return false
+    if (this._vertexIndex < this._currentLine.getNumPoints() - 1) return false
+    return true
+  }
+  hasNext () {
+    if (this._componentIndex >= this._numLines) return false
+    if (this._componentIndex === this._numLines - 1 && this._vertexIndex >= this._currentLine.getNumPoints()) return false
+    return true
+  }
+  interfaces_ () {
+    return []
+  }
+  getClass () {
+    return LinearIterator
+  }
+  static segmentEndVertexIndex (loc) {
+    if (loc.getSegmentFraction() > 0.0) return loc.getSegmentIndex() + 1
+    return loc.getSegmentIndex()
+  }
 }
-extend(LinearIterator.prototype, {
-	getComponentIndex: function () {
-		return this._componentIndex;
-	},
-	getLine: function () {
-		return this._currentLine;
-	},
-	getVertexIndex: function () {
-		return this._vertexIndex;
-	},
-	getSegmentEnd: function () {
-		if (this._vertexIndex < this.getLine().getNumPoints() - 1) return this._currentLine.getCoordinateN(this._vertexIndex + 1);
-		return null;
-	},
-	next: function () {
-		if (!this.hasNext()) return null;
-		this._vertexIndex++;
-		if (this._vertexIndex >= this._currentLine.getNumPoints()) {
-			this._componentIndex++;
-			this.loadCurrentLine();
-			this._vertexIndex = 0;
-		}
-	},
-	loadCurrentLine: function () {
-		if (this._componentIndex >= this._numLines) {
-			this._currentLine = null;
-			return null;
-		}
-		this._currentLine = this._linearGeom.getGeometryN(this._componentIndex);
-	},
-	getSegmentStart: function () {
-		return this._currentLine.getCoordinateN(this._vertexIndex);
-	},
-	isEndOfLine: function () {
-		if (this._componentIndex >= this._numLines) return false;
-		if (this._vertexIndex < this._currentLine.getNumPoints() - 1) return false;
-		return true;
-	},
-	hasNext: function () {
-		if (this._componentIndex >= this._numLines) return false;
-		if (this._componentIndex === this._numLines - 1 && this._vertexIndex >= this._currentLine.getNumPoints()) return false;
-		return true;
-	},
-	interfaces_: function () {
-		return [];
-	},
-	getClass: function () {
-		return LinearIterator;
-	}
-});
-LinearIterator.segmentEndVertexIndex = function (loc) {
-	if (loc.getSegmentFraction() > 0.0) return loc.getSegmentIndex() + 1;
-	return loc.getSegmentIndex();
-};
