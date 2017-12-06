1a9635e2e344e4a9078784d1ad235fd7fdfc1dc6
Denis <carriere.denis@gmail.com>
2017-12-06 13:33:32 -0500

    Refactor HashMap

--- src/java/util/HashMap.js
+++ src/java/util/HashMap.js
@@ -1,9 +1,6 @@
 import ArrayList from './ArrayList'
 import MapInterface from './Map'
 import HashSet from './HashSet'
-import MapPolyfill from '../../Map'
-
-let MapImpl = typeof Map === 'undefined' || !Map.prototype.values ? MapPolyfill : Map
 
 /**
  * @see http://download.oracle.com/javase/6/docs/api/java/util/HashMap.html
@@ -12,56 +9,53 @@ let MapImpl = typeof Map === 'undefined' || !Map.prototype.values ? MapPolyfill 
  * @constructor
  * @private
  */
-export default function HashMap () {
-  /**
-   * @type {Object}
-   * @private
-  */
-  this.map_ = new MapImpl()
-}
-HashMap.prototype = new MapInterface()
-
-/**
- * @override
- */
-HashMap.prototype.get = function (key) {
-  return this.map_.get(key) || null
-}
-
-/**
- * @override
- */
-HashMap.prototype.put = function (key, value) {
-  this.map_.set(key, value)
-  return value
-}
-
-/**
- * @override
- */
-HashMap.prototype.values = function () {
-  const arrayList = new ArrayList()
-  const it = this.map_.values()
-  let o = it.next()
-  while (!o.done) {
-    arrayList.add(o.value)
-    o = it.next()
+export default class HashMap extends MapInterface {
+  constructor () {
+    super()
+    this.map_ = new Map()
   }
-  return arrayList
-}
+  /**
+   * @override
+   */
+  get (key) {
+    return this.map_.get(key) || null
+  }
 
-/**
- * @override
- */
-HashMap.prototype.entrySet = function () {
-  const hashSet = new HashSet()
-  this.map_.entries().forEach(entry => hashSet.add(entry))
-  return hashSet
-}
+  /**
+   * @override
+   */
+  put (key, value) {
+    this.map_.set(key, value)
+    return value
+  }
 
-/**
- * @override
- */
-HashMap.prototype.size = function () {
-  return this.map_.size()
+  /**
+   * @override
+   */
+  values () {
+    const arrayList = new ArrayList()
+    const it = this.map_.values()
+    let o = it.next()
+    while (!o.done) {
+      arrayList.add(o.value)
+      o = it.next()
+    }
+    return arrayList
+  }
+
+  /**
+   * @override
+   */
+  entrySet () {
+    const hashSet = new HashSet()
+    this.map_.entries().forEach(entry => hashSet.add(entry))
+    return hashSet
+  }
+
+  /**
+   * @override
+   */
+  size () {
+    return this.map_.size()
+  }
 }
