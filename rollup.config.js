dccecab3f4d8863c2d8e3e37fc53d7ce9cae75c6
Denis <carriere.denis@gmail.com>
2017-12-07 03:10:57 -0500

    Drop babel from build

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,27 @@
-import babel from 'rollup-plugin-babel'
+import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
 
 export default [{
   input: 'src/index.js',
-  extend: true,
   output: {
     file: 'jsts.mjs',
     format: 'es'
-  },
-  plugins: []
+  }
 },
 {
   input: 'src/index.js',
-  extend: true,
   output: {
     file: 'jsts.js',
     format: 'cjs'
   },
-  plugins: [babel()]
+  plugins: [buble()]
 },
 {
   input: 'src/index.js',
-  extend: true,
   output: {
     file: 'jsts.min.js',
     format: 'umd',
     name: 'jsts'
   },
-  plugins: [babel(), uglify()]
+  plugins: [buble(), uglify()]
 }]
