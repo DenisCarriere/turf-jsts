bc28210e9ab900816426005d6edfda8b2e1b94ff
Olavi Mustanoja <tilastokeskus@gmail.com>
2018-01-25 12:44:03 +0200

    Implement IllegalArgumentException properly

--- src/java/lang/IllegalArgumentException.js
+++ src/java/lang/IllegalArgumentException.js
@@ -1,1 +1,8 @@
-export default class IllegalArgumentException {}
+export default class IllegalArgumentException extends Error {
+	constructor (message) {
+		super(message)
+		this.name = 'IllegalArgumentException'
+		this.message = message
+		this.stack = (new Error()).stack
+	}
+}
